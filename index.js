0665dcfbf97e5dbd258a980ea934fcf0ac1babe8
AKBORRAH <rkhmv230608@gmail.com>
2025-07-03 00:36:13 +0500

    first site commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import TelegramBot from "node-telegram-bot-api";
 
+
+
+
 const token = "7827745558:AAE3AcIci_O8LRR5eHKr4Y7Bu2bH6J3J16Q"
+const webApp = 'https://akborrah.github.io/tg-web-app-react/'
 
 
 const bot = new TelegramBot(token, { polling: true });
@@ -8,14 +12,13 @@ const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\/start/, async (msg) => {
     const chatId = msg.chat.id
     if(msg.text === '/start') {
-        const options = {
-        
-        reply_markup: {
-            inline_keyboard: [
-                [{text: "Buy 🎁", callback_data: '/'}]
-            ]
+        const options = {   
+            reply_markup: {
+                keyboard: [
+                    [{text: "Buy 🎁", web_app: {url: webApp}}]
+                ]
+            }
         }
-    }
-    await bot.sendMessage(chatId, 'Привет! Добро пожаловать в магазин 🎁', options);
+        await bot.sendMessage(chatId, 'Привет! Добро пожаловать в магазин 🎁', options);
     }
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { use } from "react";
+
 function App() {
+
+    const tg = window.Telegram.WebApp;
+
+    const onClose = () => {
+    tg.close()
+    }
+
+    useEffect(() => {
+        tg.ready()
+    }, [])
+
     return (
         <div>
-            asdasdsadas
+            asdasdsadasASDASDASDSADAS
         </div>
     );
 }
